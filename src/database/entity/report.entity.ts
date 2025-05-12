29ff93ea74342106ec151f52ff281901e864bd36
Destructzz <sklyarenko_2006@mail.ru>
2025-05-12 15:02:57 +0300

    fix merge bugs

--- src/database/entity/report.entity.ts
+++ src/database/entity/report.entity.ts
@@ -1,10 +1,5 @@
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
-import { User } from "./user.entity";
-import { Status } from "./status.entity";
-
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { User } from './user.entity';
-import { Support } from './support.entity';
 import { Status } from './status/status.entity';
 
 @Entity()
@@ -21,19 +16,11 @@ export class Report {
   @Column({ type: Date })
   status_updated_at: Date;
 
-    @Column({type : Date})
-    complited_at : Date
-    
-    @ManyToOne(() => User, (user) => user.reports, {eager : true})
-    user : User
   @Column({ type: Date })
   complited_at: Date;
 
   @ManyToOne(() => User, (user) => user.reports, { eager: true })
   user: User;
-
-  @ManyToOne(() => Support, (support) => support.reports, { eager: true })
-  support: Support;
 
   @ManyToOne(() => Status, (status) => status.reports, { eager: true })
   status: Status;
